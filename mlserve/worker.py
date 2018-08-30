a0ef5c7cfe8fc52e733fe5fcd3350e822b1256c1
Nikolay Novik <nickolainovik@gmail.com>
2018-08-31 00:12:11 +0300

    test warm cache method

--- mlserve/worker.py
+++ mlserve/worker.py
@@ -1,30 +1,37 @@
+import json
+from typing import Dict, Any, List, Optional
+
 import numpy as np
 import pandas as pd
-import json
-from typing import Dict, Any, List
 from .utils import ModelDescriptor
 from .loaders import get_loader
 
 
 # TODO: add structural typing with predict method instead
 # of Any
-_models: Dict[str, Any] = {}
+Cache = Dict[str, Any]
+_models: Cache = {}
 
 
-def warm(models: List[ModelDescriptor]) -> bool:
+def warm(models: List[ModelDescriptor], cache: Optional[Cache]=None) -> bool:
     global _models
+    cache = cache if cache is not None else _models
+
     for model in models:
         loader = get_loader(model.loader)
-        if model.name not in _models:
+        if model.name not in cache:
             pipeline = loader(str(model.model_path))
-            _models[model.name] = pipeline
+            cache[model.name] = pipeline
     return True
 
 
-def predict(model_name: str, raw_data: bytes) -> List[float]:
+def predict(
+        model_name: str, raw_data: bytes, cache: Optional[Cache]=None
+) -> List[float]:
+    cache = cache if cache is not None else _models
     # TODO: wrap this call into try except
     df = pd.DataFrame(json.loads(raw_data))
-    model = _models[model_name]
+    model = cache[model_name]
     results: List[float]
     if hasattr(model, 'predict_proba'):
         results = model.predict_proba(df)
